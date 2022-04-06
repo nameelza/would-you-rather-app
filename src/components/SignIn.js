e502a9337d61a5b1e94ca07ac8a036c25403c555
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-06 16:46:57 -0700

    signInComponent

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,13 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
 
 class SignIn extends Component {
   render() {
+      const { users, loading } = this.props;
     return (
-      <div>
-        <h3>Sign In</h3>
-      </div>
+      <Fragment>
+        {loading ? (
+          <h3>Loading...</h3>
+        ) : (
+          <div>
+            <h1>Sign In</h1>
+            <form>
+              <select>
+                <option value="">Select User</option>
+                {Object.keys(users).map((id) => (
+                    <option key={id} value={users[id]}>
+                        {users[id].name}
+                    </option>
+                ))}
+              </select>
+            </form>
+          </div>
+        )}
+      </Fragment>
     );
   }
 }
 
-export default SignIn;
+function mapStateToProps({ users }) {
+  return {
+    users,
+    loading: Object.keys(users).length === 0,
+  };
+}
+
+export default connect(mapStateToProps)(SignIn);
