c22ca452a7f7e961da29cbaef02821bf3d580d6f
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-06 14:01:29 -0700

    APPjs

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 // import { handleSignIn } from "../actions/shared";
 import { getUsers } from "../actions/users";
-import logo from "../logo.svg";
+// import logo from "../logo.svg";
 import "../App.css";
+import SignIn from "./SignIn";
 
 class App extends Component {
   componentDidMount() {
@@ -12,22 +13,25 @@ class App extends Component {
   render() {
     console.log("APP PROPS", this.props);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <SignIn />
+      </Fragment>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
     );
   }
 }
---
+++ src/components/SignIn.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SignIn extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Sign In</h3>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
