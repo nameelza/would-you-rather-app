94f2fb9aa2541fa3ece2d641467e60d051d803d3
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-07 15:20:26 -0700

    sharedAction

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -10,12 +10,16 @@ export function handleSignIn(user) {
   return (dispatch) => {
     return _getQuestions().then((questions) => {
       dispatch(setAuthedUser(user.id));
+
+      // filter out questions that the user hasn't answered
       const unanswered = Object.entries(questions).filter(([key, value]) => {
         return !(key in user.answers);
       });
       dispatch(receiveUnansweredQuestions(Object.fromEntries(unanswered)));
+
+      // filter out questions that the user has answered
       const answered = Object.entries(questions).filter(([key, value]) => {
-        return (key in user.answers);
+        return key in user.answers;
       });
       dispatch(receiveAnsweredQuestions(Object.fromEntries(answered)));
     });
