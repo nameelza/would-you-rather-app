388d70330872ff246aed9ee1339d51920036e5ad
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-08 16:22:19 -0700

    Navjs

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,10 @@
 import React, { Component, Fragment } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { connect } from "react-redux";
 import { getUsers } from "../actions/users";
 import "../App.css";
 import "../index.css";
 import SignIn from "./SignIn";
 import CardsList from "./CardsList";
-import New from "./New";
-import LeaderBoard from "./LeaderBoard";
-import Nav from "./Nav";
 
 class App extends Component {
   componentDidMount() {
@@ -17,26 +13,17 @@ class App extends Component {
   render() {
     console.log("APP PROPS", this.props);
     return (
-      <Router>
-        <Fragment>
-          <div className="container">
-            <Nav />
-              <Routes>
-                <Route path="/" exact element={<CardsList />} />
-                <Route path="/new" element={<New />} />
-                <Route path="/leaderboard" element={<LeaderBoard />} />
-                <Route path="/signin" element={<SignIn />} />
-              </Routes>
-          </div>
-        </Fragment>
-      </Router>
+      <Fragment>
+        <div className="container">
+          {this.props.singedIn ? <SignIn /> : <CardsList />}
+        </div>
+      </Fragment>
     );
   }
 }
 
-function mapStateToProps({ users, authedUser }) {
+function mapStateToProps({ authedUser }) {
   return {
-    users,
     singedIn: authedUser !== null,
   };
 }
