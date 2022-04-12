29786f1e32230a0c19b3af5e54547440421cba2d
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-12 15:08:48 -0700

    css

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,14 @@ import { connect } from "react-redux";
 
 class Card extends Component {
   render() {
-    const { author, optionOne, optionTwo } = this.props.question;
+    const { author, optionOne } = this.props.question;
 
     return (
       <div className="card">
         <p>{author} asks:</p>
-        <p>Would you rather {optionOne.text} or {optionTwo.text}</p>
+        <h3>Would you rather</h3>
+        <p>{optionOne.text} or ...</p>
+        <button>View Poll</button>
       </div>
     );
   }
