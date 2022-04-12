6d64970defecf79621d6af4c52cf2e2c0e70911b
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-12 16:36:17 -0700

    css

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,13 +7,35 @@ class Card extends Component {
 
     return (
       <div className="card">
-        <p>{author} asks:</p>
-        <h3>Would you rather</h3>
-        <p>{optionOne.text} or ...</p>
-        <button>View Poll</button>
+        <div className="card-header">
+          <p>{author} asks:</p>
+        </div>
+        <div id="user-card">
+          <img
+            src={this.props.avatar}
+            alt={`Avatar of ${author.name}`}
+            className="avatar"
+          />
+          <div>
+            <div>
+              <h3>Would you rather</h3>
+              <p>{optionOne.text} or ...</p>
+            </div>
+            <div>
+              <button>View Poll</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default connect()(Card);
+function mapStateToProps({ users }, { question }) {
+  const author = users[question.author];
+  return {
+    avatar: author.avatarURL,
+  };
+}
+
+export default connect(mapStateToProps)(Card);
