d42abe6ffa7d77b840db6c150aa56e6f913e9b22
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-14 14:49:01 -0700

    sortUsers

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -8,14 +8,22 @@ class LeaderBoard extends Component {
       <div>
         <h1 className="center">Leader Board</h1>
         <ul>
-          {Object.keys(this.props.users).map((id) => {
-            const user = this.props.users[id];
-            return (
-              <li key={id}>
-                <UserCard user={user} />
-              </li>
+          {Object.keys(this.props.users)
+            .sort(
+              (a, b) =>
+                this.props.users[b].questions.length +
+                Object.keys(this.props.users[b].answers).length -
+                (this.props.users[a].questions.length +
+                  Object.keys(this.props.users[a].answers).length)
             )
-          })}
+            .map((id) => {
+              const user = this.props.users[id];
+              return (
+                <li key={id}>
+                  <UserCard user={user} />
+                </li>
+              );
+            })}
         </ul>
       </div>
     );
