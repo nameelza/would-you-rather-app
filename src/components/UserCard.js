f102324ff9c69ca5838e9e31b9fe5c78ea96918e
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-20 15:18:39 -0700

    userCard

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -5,18 +5,18 @@ function UserCard({ user }) {
   const totalAnswers = Object.keys(answers).length;
   const totalQuestions = questions.length;
   const score = totalAnswers + totalQuestions;
+
   return (
     <div className="card" id="user-card">
-      <img src={avatarURL} alt={`Avatar of ${name}`} className="avatar"/>
+      <img src={avatarURL} alt={`Avatar of ${name}`} className="avatar" />
       <div>
         <h3>{name}</h3>
         <p> Created questions {totalQuestions}</p>
         <p> Answered questions {totalAnswers}</p>
       </div>
       <div>
-          <p>Score {score}</p>
+        <p>Score {score}</p>
       </div>
-      
     </div>
   );
 }
