95cc302c3e52c7b9c25a9d45cdfd68f0798fac8a
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-20 14:39:07 -0700

    css

--- src/components/New.js
+++ src/components/New.js
@@ -36,7 +36,7 @@ class New extends Component {
       <div className="border">
         <h1 className="center">Create New Question</h1>
         <p>Complete the question:</p>
-        <h2 className="subheading">Would you rather...</h2>
+        <h2 id="subheading">Would you rather...</h2>
         <form onSubmit={this.handleSubmit}>
           <input
             type="text"
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -29,7 +29,7 @@ function SignIn({ users, loading, dispatch }) {
       ) : (
         <div className="border">
           <h1 className="center">Welcome to Would You Rather app</h1>
-          <p className="subheading">Please sign in to continue</p>
+          <p className="center" id="subheading">Please sign in to continue</p>
           <form onSubmit={handleSubmit}>
             <select value={value} onChange={handleChange} className="input">
               <option value="" disabled>
