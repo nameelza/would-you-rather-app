2e75b3f3197be43584940a14de0534cf50e2ca2b
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-12 16:09:16 -0700

    wyra

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-import { getUsers } from "../actions/users";
+import { getUsers } from "./actions/users";
 import { Routes, Route } from "react-router-dom";
 import LoadingBar from "react-redux-loading";
 import "../App.css";
 import "../index.css";
-import SignIn from "./SignIn";
-import CardsList from "./CardsList";
-import New from "./New";
-import LeaderBoard from "./LeaderBoard";
-import Nav from "./Nav";
-import CardPoll from "./CardPoll";
-import PrivateRoute from "./PrivateRoute";
+import SignIn from "./components/SignIn";
+import CardsList from "./components/CardsList";
+import New from "./components/New";
+import LeaderBoard from "./components/LeaderBoard";
+import Nav from "./components/Nav";
+import CardPoll from "./components/CardPoll";
+import PrivateRoute from "./components/PrivateRoute";
 
 class App extends Component {
   componentDidMount() {
@@ -25,7 +25,7 @@ class App extends Component {
         <div className="container">
           <Routes>
             <Route path="/signIn" element={<SignIn />} />
-            <PrivateRoute exact path="/" element={<CardsList />} />
+            <PrivateRoute path="/" element={<CardsList />}  />
             <PrivateRoute path="/newQuestion" element={<New />} />
             <PrivateRoute path="/leaderBoard" element={<LeaderBoard />} />
             <PrivateRoute path="/card/:id" element={<CardPoll />} />
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,5 +1,29 @@
-function PrivateRoute() {
-  return null;
+import { connect } from "react-redux";
+import { Navigate, Route } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { isLoggedIn } = this.props;
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return isLoggedIn ? (
+          <Component {...props} />
+        ) : (
+          <Navigate
+            to={{ pathname: "/signIn", state: { from: props.location } }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+function mapStateToProps({ authedUser }) {
+  return {
+    isLoggedIn: authedUser !== null,
+  };
 }
 
-export default PrivateRoute;
+export default connect(mapStateToProps)(PrivateRoute);
